99edbc8eee89421ed9c50eda80cf44558d142ea3
Sahat Yalkabov <sakhat@gmail.com>
2013-06-08 05:25:40 -0400

    added race icons and nice progress bar on top

--- public/js/app.js
+++ public/js/app.js
@@ -405,3 +405,18 @@ $(document).on("ready", function () {
 
 
 });
+
+$(document).ajaxStart(function() {
+    //only add progress bar if added yet.
+    if ($("#progress").length === 0) {
+        $("body").append($("<div><dt/><dd/></div>").attr("id", "progress"));
+        $("#progress").width((50 + Math.random() * 30) + "%");
+    }
+});
+
+$(document).ajaxComplete(function() {
+    //End loading animation
+    $("#progress").width("101%").delay(200).fadeOut(400, function() {
+        $(this).remove();
+    });
+});
