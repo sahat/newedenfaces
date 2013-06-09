e3cc03eb484cc4e0affc501045ed04a17d4e42d8
Sahat Yalkabov <sakhat@gmail.com>
2013-06-09 13:43:47 -0400

    cleaned up files and folder

--- public/js/views/characterview.js
+++
@@ -1,9 +0,0 @@
-App.CharacterView = Backbone.View.extend({
-
-    render: function () {
-        this.$el.html(this.template(this.model.toJSON()));
-        return this;
-    }
-
-});
-
--- public/js/views/contact.js
+++
@@ -1,8 +0,0 @@
-App.ContactView = Backbone.View.extend({
-
-    render:function () {
-        this.$el.html(this.template());
-        return this;
-    }
-
-});
