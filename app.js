3c69f83172554bc62f0d5fee761e33fefe13bf4f
Sahat Yalkabov <sakhat@gmail.com>
2013-06-18 20:54:36 -0400

    sorting by wins everywhere

--- app.js
+++ app.js
@@ -594,8 +594,8 @@ var NewEdenFaces = function() {
     app.get('/api/leaderboard', function(req, res) {
       Character
       .find()
-      .limit(14)
       .sort('-wins')
+      .limit(14)
       .exec(function(err, characters) {
         if (err) {
           console.log(err);
@@ -896,26 +896,26 @@ var NewEdenFaces = function() {
     });
 
 
-    app.post('/api/feedback', function(req, res) {
-      var sendgrid = new SendGrid(config.sendgrid_user, config.sendgrid_key);
-      var characterName = req.body.characterName;
-      var message = req.body.message;
-      var uiRating = req.body.uiRating;
-      var text = 'From: ' + characterName + '.' + 'User Interface: ' +
-                  uiRating + '.' + 'Message: ' + message + '.';
+    // app.post('/api/feedback', function(req, res) {
+    //   var sendgrid = new SendGrid(config.sendgrid_user, config.sendgrid_key);
+    //   var characterName = req.body.characterName;
+    //   var message = req.body.message;
+    //   var uiRating = req.body.uiRating;
+    //   var text = 'From: ' + characterName + '.' + 'User Interface: ' +
+    //               uiRating + '.' + 'Message: ' + message + '.';
       
-      sendgrid.send({
-        to: 'sakhat@gmail.com',
-        from: 'aura@neweden.com',
-        subject: 'Site Feedback',
-        text: text
-      }, function(success, message) {
-        if (!success) {
-          console.log(message);
-        }
-        res.send('Email has been sent successfully');
-      });
-    });
+    //   sendgrid.send({
+    //     to: 'sakhat@gmail.com',
+    //     from: 'aura@neweden.com',
+    //     subject: 'Site Feedback',
+    //     text: text
+    //   }, function(success, message) {
+    //     if (!success) {
+    //       console.log(message);
+    //     }
+    //     res.send('Email has been sent successfully');
+    //   });
+    // });
 
 
     // PushState redirects
@@ -924,9 +924,9 @@ var NewEdenFaces = function() {
       res.redirect('/#add');
     });
 
-    app.get('/feedback', function(req, res) {
-      res.redirect('/#feedback');
-    });
+    // app.get('/feedback', function(req, res) {
+    //   res.redirect('/#feedback');
+    // });
 
     app.get('/top', function(req, res) {
       res.redirect('/#top');
