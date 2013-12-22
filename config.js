14295d6486ffa54781173445194c10027c3a8a93
Sahat Yalkabov <sakhat@gmail.com>
2013-12-21 22:50:03 -0500

    config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  sendgrid_user: 'sahat',
+  sendgrid_key: 'sendgridEVE',
+  mongoose: 'mongodb://sahat:newedenfaces@ds029638.mongolab.com:29638/newedenfaces',
+  secretCode: 'insertcoin'
+};
