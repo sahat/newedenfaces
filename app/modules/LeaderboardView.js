1b4b7453b99844f60695cacee0da220f0693dcf1
Sahat Yalkabov <sakhat@gmail.com>
2013-12-22 23:53:30 -0500

    Leaderboard view ported to require.js

---
+++ app/modules/LeaderboardView.js
@@ -0,0 +1,22 @@
+define(function(require, exports, module) {
+  var _ = require('underscore');
+  var $ = require('jquery');
+  var Backbone = require('backbone');
+  var LeaderboardItemView = require('modules/LeaderboardItemView');
+
+  var LeaderboardView = Backbone.View.extend({
+    tagName: 'ul',
+
+    className: 'inline',
+
+    render: function() {
+      this.collection.each(function(character) {
+        var leaderboardItemView = new App.Views.LeaderboardItem({ model: character });
+        this.$el.append(leaderboardItemView.render().el);
+      }, this);
+      return this;
+    }
+  });
+
+  return LeaderboardView;
+});
