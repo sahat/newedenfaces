5a5e87a708c42600117b893507a0353a6c0b6436
Sahat Yalkabov <sakhat@gmail.com>
2013-12-23 01:48:20 -0500

    Removed library references in index.html, added toastr, typeahead and magnific popup to require.js config

--- app/config.js
+++ app/config.js
@@ -5,13 +5,22 @@ require.config({
     'underscore': 'vendor/lodash/dist/lodash.underscore',
     'jquery': 'vendor/jquery/jquery',
     'backbone': 'vendor/backbone/backbone',
-    'text': 'vendor/requirejs-text/text'
+    'text': 'vendor/requirejs-text/text',
+    'toastr': 'vendor/toastr',
+    'typeahead': 'vendor/typeahead.js/dist/typeahead',
+    'magnific-popup': 'vendor/magnific-popup/dist/jquery.magnific-popup'
   },
 
   shim: {
     'backbone': {
       deps: ['jquery', 'underscore'],
       exports: 'Backbone'
+    },
+    'magnific-popup': {
+      deps: ['jquery']
+    },
+    'typeahead': {
+      deps: ['jquery']
     }
   }
 });
