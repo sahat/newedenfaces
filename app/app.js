d9a926eac10b837c179b718874522865f2673135
Sahat Yalkabov <sakhat@gmail.com>
2013-12-23 02:43:04 -0500

    Ported youtube-style loading ajax bar to require.js

--- app/app.js
+++ app/app.js
@@ -4,6 +4,35 @@ define(function(require, exports, module) {
   var Backbone = require('backbone');
   var dropdown = require('bootstrap-dropdown');
 
+  $(document).on("ready", function () {
+
+    $(document).on('click', 'a:not([data-bypass])', function(e){
+      var href = $(this).prop('href');
+      var root = location.protocol + '//' + location.host + '/';
+      if (root === href.slice(0,root.length)){
+        e.preventDefault();
+        Backbone.history.navigate(href.slice(root.length), true);
+      }
+    });
+
+  });
+
+  $(document).ajaxStart(function() {
+    //only add progress bar if added yet.
+    if ($("#progress").length === 0) {
+      $("body").append($("<div><dt/><dd/></div>").attr("id", "progress"));
+      $("#progress").width((50 + Math.random() * 30) + "%");
+    }
+  });
+
+  $(document).ajaxComplete(function() {
+    $('.spinner').css('z-index', -1);
+    //End loading animation
+    $("#progress").width("101%").delay(200).fadeOut(400, function() {
+      $(this).remove();
+    });
+  });
+
   // Alias the module for easier identification.
   var app = module.exports;
 
