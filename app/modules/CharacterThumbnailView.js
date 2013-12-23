edb443452a9e02e3fbf35c22111903095f6ed430
Sahat Yalkabov <sakhat@gmail.com>
2013-12-22 23:42:23 -0500

    Moved character thumbnail view to require.js

---
+++ app/modules/CharacterThumbnailView.js
@@ -0,0 +1,32 @@
+define(function(require, exports, module) {
+  var _ = require('underscore');
+  var $ = require('jquery');
+  var Backbone = require('backbone');
+  var CharacterThumbnailTemplate = require('text!templates/character-thumbnail');
+
+  var CharacterThumbnailView = Backbone.View.extend({
+    tagName: 'li',
+
+    className: 'span6',
+
+    template: _.template(CharacterThumbnailTemplate),
+
+    events: {
+      'click img': 'updateWinner'
+    },
+
+    render: function () {
+      this.$el.html(this.template(this.model.toJSON()));
+      this.$el.find('img').on('dragstart', function(e) {
+        e.preventDefault();
+      });
+      return this;
+    },
+
+    updateWinner: function() {
+      this.model.set('wins', this.model.get('wins') + 1);
+    }
+  });
+
+  return CharacterThumbnailView;
+});
