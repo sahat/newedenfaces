70538de5fc00d4a896226bb432a31ae057259e23
Sahat Yalkabov <sakhat@gmail.com>
2013-12-23 01:16:16 -0500

    Updated all references in router to include require.js modules

--- app/router.js
+++ app/router.js
@@ -1,13 +1,24 @@
 define(function(require, exports, module) {
-  var Backbone = require("backbone");
+  var _ = require('underscore');
+  var $ = require('jquery');
+  var Backbone = require('backbone');
+  var CharacterModel = require('modules/CharacterModel');
+  var CharacterCollection = require('modules/CharacterCollection');
+  var CharacterCollectionView = require('modules/CharacterCollectionView');
+  var CharacterSummaryView = require('modules/CharacterSummaryView');
+  var SearchView = require('modules/SearchView');
+  var LeaderboardView = require('modules/LeaderboardView');
+  var HomeView = require('modules/HomeView');
+  var WrongGenderView = require('modules/WrongGenderView');
+  var AddCharacterView = require('modules/AddCharacterView');
 
   module.exports = Backbone.Router.extend({
     initialize: function() {
-      var characters = new App.Collections.Characters();
+      var characters = new CharacterCollection();
       characters.fetch({
         url: '/api/characters/all',
         success: function(data) {
-          var searchView = new App.Views.Search({
+          var searchView = new SearchView({
             collection: characters
           });
         }
@@ -15,10 +26,10 @@ define(function(require, exports, module) {
       characters.fetch({
         url: '/api/leaderboard',
         success: function(data) {
-          App.Views.leaderboardView = new App.Views.Leaderboard({
+          var leaderboardView = new LeaderboardView({
             collection: data
           });
-          $('.footer #leaderboard').html(App.Views.leaderboardView.render().el);
+          $('.footer #leaderboard').html(leaderboardView.render().el);
         }
       });
     },
@@ -40,193 +51,193 @@ define(function(require, exports, module) {
     },
 
     home: function() {
-      var characters = new App.Collections.Characters();
+      var characters = new CharacterCollection();
       characters.fetch({
         success: function(data) {
 
-          App.Views.homeView = new App.Views.Home({
+          var homeView = new HomeView({
             collection: data
           });
 
-          $('#content').html(App.Views.homeView.render().el);
-          App.Views.homeView.selectMenuItem('home-menu');
+          $('#content').html(homeView.render().el);
+          homeView.selectMenuItem('home-menu');
         }
       });
     },
 
     topBloodline: function(race, bloodline) {
-      var characters = new App.Collections.Characters();
+      var characters = new CharacterCollection();
       characters.fetch({
         url: '/api/characters/top?race=' + race + '&bloodline=' + bloodline,
         success: function(data) {
-          App.Views.charactersView = new App.Views.Characters({
+          var characterCollectionView = new CharacterCollectionView({
             collection: characters
           });
           $('#content').html('<div class="panel"></div>');
-          $('.panel').html(App.Views.charactersView.render().el);
-          App.Views.charactersView.selectMenuItem('top-menu');
+          $('.panel').html(characterCollectionView.render().el);
+          characterCollectionView.selectMenuItem('top-menu');
         }
       });
     },
 
     topCharacters: function() {
-      var characters = new App.Collections.Characters();
+      var characters = new CharacterCollection();
       characters.fetch({
         url: '/api/characters/top',
         success: function(data) {
-          App.Views.charactersView = new App.Views.Characters({
+          var characterCollectionView = new CharacterCollectionView({
             collection: characters
           });
           $('#content').html('<div class="panel"></div>');
-          $('.panel').html(App.Views.charactersView.render().el);
-          App.Views.charactersView.selectMenuItem('top-menu');
+          $('.panel').html(characterCollectionView.render().el);
+          characterCollectionView.selectMenuItem('top-menu');
         }
       });
     },
 
     wrongGender: function() {
-      var characters = new App.Collections.Characters();
+      var characters = new CharacterCollection();
       characters.fetch({
         url: '/api/characters/wrong-gender',
         success: function(data) {
-          App.Views.wrongGenderView = new App.Views.WrongGender({
+          var wrongGenderView = new WrongGenderView({
             collection: characters
           });
-          $('#content').html(App.Views.wrongGenderView.render().el);
-          App.Views.wrongGenderView.selectMenuItem('top-menu');
+          $('#content').html(wrongGenderView.render().el);
+          wrongGenderView.selectMenuItem('top-menu');
         }
       });
     },
 
     hallOfShame: function() {
-      var characters = new App.Collections.Characters();
+      var characters = new CharacterCollection();
       characters.fetch({
         url: '/api/characters/shame',
         success: function() {
-          App.Views.charactersView = new App.Views.Characters({
+          var characterCollectionView = new CharacterCollectionView({
             collection: characters
           });
           $('#content').html('<div class="panel"></div>');
-          $('.panel').html(App.Views.charactersView.render().el);
-          App.Views.charactersView.selectMenuItem('hall-of-shame-menu');
+          $('.panel').html(characterCollectionView.render().el);
+          characterCollectionView.selectMenuItem('hall-of-shame-menu');
         }
       });
     },
 
     topRace: function(race) {
-      var characters = new App.Collections.Characters();
+      var characters = new CharacterCollection();
       characters.fetch({
         url: '/api/characters/top/?race=' + race,
         success: function(data) {
-          App.Views.charactersView = new App.Views.Characters({
+          var characterCollectionView = new CharacterCollectionView({
             collection: characters
           });
           $('#content').html('<div class="panel"></div>');
-          $('.panel').html(App.Views.charactersView.render().el);
-          App.Views.charactersView.selectMenuItem('top-menu');
+          $('.panel').html(characterCollectionView.render().el);
+          characterCollectionView.selectMenuItem('top-menu');
         }
       });
     },
 
     maleRace: function(race) {
-      var characters = new App.Collections.Characters();
+      var characters = new CharacterCollection();
       characters.fetch({
         url: '/api/characters/top?gender=male&race=' + race,
         success: function(data) {
-          App.Views.charactersView = new App.Views.Characters({
+          var characterCollectionView = new CharacterCollectionView({
             collection: characters
           });
           $('#content').html('<div class="panel"></div>');
-          $('.panel').html(App.Views.charactersView.render().el);
-          App.Views.charactersView.selectMenuItem('top-male');
+          $('.panel').html(characterCollectionView.render().el);
+          characterCollectionView.selectMenuItem('top-male');
         }
       });
     },
 
     femaleRace: function(race) {
-      var characters = new App.Collections.Characters();
+      var characters = new CharacterCollection();
       characters.fetch({
         url: '/api/characters/top?gender=female&race=' + race,
         success: function(data) {
-          App.Views.charactersView = new App.Views.Characters({
+          var characterCollectionView = new CharacterCollectionView({
             collection: characters
           });
           $('#content').html('<div class="panel"></div>');
-          $('.panel').html(App.Views.charactersView.render().el);
-          App.Views.charactersView.selectMenuItem('top-female');
+          $('.panel').html(characterCollectionView.render().el);
+          characterCollectionView.selectMenuItem('top-female');
         }
       });
     },
 
 
     maleBloodline: function(race, bloodline) {
-      var characters = new App.Collections.Characters();
+      var characters = new CharacterCollection();
       characters.fetch({
         url: '/api/characters/top?gender=male&race=' + race + '&bloodline=' + bloodline,
         success: function(data) {
-          App.Views.charactersView = new App.Views.Characters({
+          var characterCollectionView = new CharacterCollectionView({
             collection: characters
           });
           $('#content').html('<div class="panel"></div>');
-          $('.panel').html(App.Views.charactersView.render().el);
-          App.Views.charactersView.selectMenuItem('top-male');
+          $('.panel').html(characterCollectionView.render().el);
+          characterCollectionView.selectMenuItem('top-male');
         }
       });
     },
 
     femaleBloodline: function(race, bloodline) {
-      var characters = new App.Collections.Characters();
+      var characters = new CharacterCollection();
       characters.fetch({
         url: '/api/characters/top?gender=female&race=' + race + '&bloodline=' + bloodline,
         success: function(data) {
-          App.Views.charactersView = new App.Views.Characters({
+          var characterCollectionView = new CharacterCollectionView({
             collection: characters
           });
           $('#content').html('<div class="panel"></div>');
-          $('.panel').html(App.Views.charactersView.render().el);
-          App.Views.charactersView.selectMenuItem('top-female');
+          $('.panel').html(characterCollectionView.render().el);
+          characterCollectionView.selectMenuItem('top-female');
         }
       });
     },
 
     maleCharacters: function() {
-      var characters = new App.Collections.Characters();
+      var characters = new CharacterCollection();
       characters.fetch({
         url: '/api/characters/top?gender=male',
         success: function(data) {
-          App.Views.charactersView = new App.Views.Characters({
+          var characterCollectionView = new CharacterCollectionView({
             collection: characters
           });
           $('#content').html('<div class="panel"></div>');
-          $('.panel').html(App.Views.charactersView.render().el);
-          App.Views.charactersView.selectMenuItem('top-male');
+          $('.panel').html(characterCollectionView.render().el);
+          characterCollectionView.selectMenuItem('top-male');
         }
       });
     },
 
     femaleCharacters: function() {
-      var characters = new App.Collections.Characters();
+      var characters = new CharacterCollection();
       characters.fetch({
         url: '/api/characters/top?gender=female',
         success: function(data) {
-          App.Views.charactersView = new App.Views.Characters({
+          var characterCollectionView = new CharacterCollectionView({
             collection: characters
           });
           $('#content').html('<div class="panel"></div>');
-          $('.panel').html(App.Views.charactersView.render().el);
-          App.Views.charactersView.selectMenuItem('top-female');
+          $('.panel').html(characterCollectionView.render().el);
+          characterCollectionView.selectMenuItem('top-female');
         }
       });
     },
 
     addCharacter: function() {
-      var addCharacterView = new App.Views.AddCharacter();
+      var addCharacterView = new AddCharacterView();
       addCharacterView.selectMenuItem('add-menu');
       $('#content').html(addCharacterView.render().el);
     },
 
     characterDetails: function (id) {
-      var character = new App.Models.Character({ characterId: id });
+      var character = new CharacterModel({ characterId: id });
       character.fetch({
         error: function(err) {
           console.log(err, 'error');
@@ -234,7 +245,7 @@ define(function(require, exports, module) {
         success: function(data) {
           var winLossRatio = (data.get('wins') / (data.get('wins') + data.get('losses')) * 100).toFixed(1);
           if (isNaN(winLossRatio)) winLossRatio = 0;
-          var characterSummaryView = new App.Views.CharacterSummary({ model: data, winLossRatio: winLossRatio });
+          var characterSummaryView = new CharacterSummaryView({ model: data, winLossRatio: winLossRatio });
           $('#content').html(characterSummaryView.render().el);
           $('#wrap').css('background-image', 'url(../img/' + data.get('race') + '-bg.jpg)');
           $('.navbar').addClass('bg').addClass('bg-black');
