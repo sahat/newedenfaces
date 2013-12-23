56f7ff87536a9f0e47a51cdf41d5c7c4b530ee42
Sahat Yalkabov <sakhat@gmail.com>
2013-12-23 00:47:17 -0500

    Moved Search  view to require.js

---
+++ app/modules/SearchView.js
@@ -0,0 +1,42 @@
+define(function(require, exports, module) {
+  var _ = require('underscore');
+  var $ = require('jquery');
+  var Backbone = require('backbone');
+
+  var SearchView = Backbone.View.extend({
+    el: $('.navbar'),
+
+    initialize: function() {
+      $('#search').typeahead({
+        source: this.collection.pluck('name')
+      });
+      $('#search').attr('placeholder', this.collection.length + ' capsuleers');
+    },
+
+    events: {
+      'submit form': 'submit'
+    },
+
+    submit: function(e) {
+      e.preventDefault();
+      var input = this.$el.find('input').val();
+
+      var queryMatch = this.collection.filter(function(model) {
+        return model.get('name').toLowerCase() === input.toLowerCase();
+      });
+
+      if (!queryMatch.length) {
+        return;
+      }
+      var characterId = queryMatch[0].get('characterId');
+
+      if (characterId) {
+        Backbone.history.navigate('/characters/' + characterId, { trigger: true });
+      } else {
+        toastr.warning('No query match');
+      }
+    }
+  });
+
+  return SearchView;
+});
